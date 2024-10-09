d5942cf8bbbed3a75cde6382cac5ce094793b7cf
zia-haider9 <zia.haider@alethea.ai>
2024-10-09 19:29:48 +0500

    utils fix

--- load_env.js
+++ load_env.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 // Set the AWS credentials from environment variables
 AWS.config.update({
-	region: process.env.AWS_DEFAULT_REGION,
+	region: process.env.AWS_REGION,
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Load AWS Access Key ID from environment
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY // Load AWS Secret Access Key from environment
 });
@@ -24,21 +24,19 @@ async function loadEnvSecrets() {
 
 		const env_list = [
 			`AWS_SECRET_NAME=${process.env.AWS_SECRET_NAME}`,
-			`AWS_REGION=${process.env.AWS_DEFAULT_REGION}`,
+			`AWS_REGION=${process.env.AWS_REGION}`,
 			`AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID}`, // Add AWS Access Key ID to env list
 			`AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY}` // Add AWS Secret Access Key to env list
 		];
 
 		Object.keys(aws_env).forEach((key) => {
 			env_list.push(`${key}=${aws_env[key]}`);
-			console.log('key', key);
 		});
 
 		await fs.writeFile('.env', env_list.join('\n'), 'utf-8');
 		console.log(
 			'-------------- Finished loading secrets from AWS --------------'
 		);
-		console.log('env_list', env_list);
 	} catch (err) {
 		console.error(
 			'-------------- Error occurred during loading env from AWS --------------'
@@ -57,7 +55,7 @@ loadEnvSecrets();
 // const dotenv = require('dotenv');
 
 // dotenv.config();
-// AWS.config.update({ region: process.env.AWS_DEFAULT_REGION });
+// AWS.config.update({ region: process.env.AWS_REGION });
 // const client = new AWS.SecretsManager();
 
 // async function loadEnvSecrets() {
@@ -69,7 +67,7 @@ loadEnvSecrets();
 
 // 		const env_list = [
 // 			`AWS_SECRET_NAME=${process.env.AWS_SECRET_NAME}`,
-// 			`AWS_REGION=${process.env.AWS_DEFAULT_REGION}`
+// 			`AWS_REGION=${process.env.AWS_REGION}`
 // 		];
 
 // 		Object.keys(aws_env).forEach((key) => {
